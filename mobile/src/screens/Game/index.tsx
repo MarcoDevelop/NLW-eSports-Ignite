29196a7674704ce2cb285161782a6e2e60dcdda1
Marco Miranda <m.miranda2015@outlook.com>
2022-09-17 22:11:42 -0300

    Version 1

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, TouchableOpacity, Image, FlatList, Text } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
+
+import { Background } from '../../componets/Background';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { THEME } from '../../theme';
+import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
+import { Heading } from '../../componets/Heading';
+import { DuoMatch } from '../../componets/DuoMatch';
+import { DuoCard, DuoCardProps } from '../../componets/DuoCard';
+
+export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
+  const navigation = useNavigation();
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  function handleGoBack(){
+    navigation.goBack();
+  }
+  
+  async function getDiscordUser(adsId: string){
+    fetch(`http://192.168.3.10:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord));
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.3.10:3333/games/${game.id}/ads`)
+    .then(response => response.json())
+    .then(data => setDuos(data));
+  }, []);
+  
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+            <Image
+              source={logoImg}
+              style={styles.logo}
+            />
+          <View style={styles.right} />
+        </View>
+
+        <Image 
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading 
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
+
+        <FlatList 
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <DuoCard 
+              data={item}
+              onConnect={() => getDiscordUser(item.id)}
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda. 
+            </Text>
+          )}
+        />
+
+        <DuoMatch
+          visible={ discordDuoSelected.length > 0 } 
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
+        />
+      </SafeAreaView>
+    </Background>
+  );
+} 
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { GameCard, GameCardProps } from '../../componets/GameCard';
+import { Heading } from '../../componets/Heading';
+import { Background } from '../../componets/Background'
+
+import { styles } from './styles';
+
+export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.3.10:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, []);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps){
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image 
+          source={logoImg}
+          style={styles.logo}
+        />
+
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <GameCard
+              data={item}
+              onPress={() => handleOpenGame(item)}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+
+      </SafeAreaView>
+    </Background>
+  );
+}
